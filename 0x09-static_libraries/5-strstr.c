9fde8cddabff11e2343402f1d3839fde77f50482
OfficialSteffie <stephanieedwin33@gmail.com>
2023-05-16 07:11:54 -0700

    updated

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strste(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = hatstack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	retuurn (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed by src
+ * @dest: copy to
+ * @stc: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char src)
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(stc + 1) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < 1 ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
